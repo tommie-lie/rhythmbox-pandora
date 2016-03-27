e28fae6f971ce16089d696c4a00ac3022199d4fa
Thomas Liebetraut <thomas@tommie-lie.de>
2016-03-27 22:10:17 +0200

    Split the source class into a source for each radio station and a "parent" display page
    Managing multiple radio stations inside a single source seems complicated
    both from a sourcecode and a UI point of view. Having sub-sources for
    each radio station like the audioscrobbler plugin has seems much nicer.

--- rhythmbox-pandora/PandoraSource.py
+++ rhythmbox-pandora/PandoraSource.py
@@ -1,18 +1,33 @@
 from gi.repository import GObject, RB
 
 class PandoraEntryType(RB.RhythmDBEntryType):
+    NAME = "pandora"
     def __init__(self):
-        super(PandoraEntryType, self).__init__(name="pandora")
+        super(PandoraEntryType, self).__init__(name=PandoraEntryType.NAME)
 
-class PandoraSource(RB.StreamingSource):
+
+class PandoraSource(RB.DisplayPage):
     def __init__(self, **kwargs):
-        et = PandoraEntryType()
         super(PandoraSource, self).__init__(name="Pandora Radio",
-                                            entry_type=et,
                                             **kwargs)
-        db = self.props.shell.props.db
-        db.register_entry_type(self.props.entry_type)
 
         group = RB.DisplayPageGroup.get_by_id("library")
         self.props.shell.append_display_page(self, group)
-        self.props.shell.register_entry_type_for_source(self, self.props.entry_type)
+
+
+class PandoraRadioStationSource(RB.StreamingSource):
+    def __init__(self, parent, shell, **kwargs):
+        if not parent:
+            raise ValueError("parent argument must not be null")
+        
+        et = shell.props.db.entry_type_get_by_name(PandoraEntryType.NAME)
+        if not et:
+            et = PandoraEntryType()
+            shell.props.db.register_entry_type(et)
+        super(PandoraRadioStationSource, self).__init__(name="Pandora Radio Station",
+                                                        entry_type=et,
+                                                        parent=parent,
+                                                        shell=shell,
+                                                        **kwargs)
+        
+        self.props.shell.append_display_page(self, parent)
