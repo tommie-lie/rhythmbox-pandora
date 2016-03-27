34dbde0411c3dfa9e799d13dbe3b2b91485e03ab
Thomas Liebetraut <thomas@tommie-lie.de>
2016-03-27 22:20:06 +0200

    Hook up the account, pandora and sources stuff to display radio stations
    Now that every piece works, put all the pieces together and create a
    radio station source for each pandora radio station in the account.

--- rhythmbox-pandora/PandoraPlugin.py
+++ rhythmbox-pandora/PandoraPlugin.py
@@ -1,16 +1,25 @@
 from gi.repository import GObject, Peas
 
 from .PandoraSource import PandoraSource
+from .PandoraAccount import PandoraAccount
+from pithos import pandora
+from pithos.pandora.data import client_keys as pandora_client_keys
 
 class PandoraPlugin(GObject.Object, Peas.Activatable):
     object = GObject.property(type=GObject.Object)
 
     def __init__(self):
         super(PandoraPlugin, self).__init__()
+        self.pandora_account = PandoraAccount.get()
+        
+        username, password = self.pandora_account.get_credentials()
+        self.pandora = pandora.Pandora()
+        self.pandora.connect(pandora_client_keys['android-generic'], username, password)
 
     def do_activate(self):
         shell = self.object
-        self.source = PandoraSource(shell=shell)
+        self.source = PandoraSource(shell=shell,
+                                    plugin=self)
 
     def do_deactivate(self):
         self.source.delete_thyself()
--- rhythmbox-pandora/PandoraSource.py
+++ rhythmbox-pandora/PandoraSource.py
@@ -13,18 +13,27 @@ class PandoraSource(RB.DisplayPage):
 
         group = RB.DisplayPageGroup.get_by_id("library")
         self.props.shell.append_display_page(self, group)
+        
+        self.stations = []
+        for station in self.props.plugin.pandora.stations:
+            self.stations.append(PandoraRadioStationSource(parent=self,
+                                                           station=station,
+                                                           **kwargs))
 
 
 class PandoraRadioStationSource(RB.StreamingSource):
-    def __init__(self, parent, shell, **kwargs):
+    def __init__(self, parent, station, shell, **kwargs):
         if not parent:
             raise ValueError("parent argument must not be null")
+        if not station:
+            raise ValueError("station argument must not be null")
         
+        self.station = station
         et = shell.props.db.entry_type_get_by_name(PandoraEntryType.NAME)
         if not et:
             et = PandoraEntryType()
             shell.props.db.register_entry_type(et)
-        super(PandoraRadioStationSource, self).__init__(name="Pandora Radio Station",
+        super(PandoraRadioStationSource, self).__init__(name=station.name,
                                                         entry_type=et,
                                                         parent=parent,
                                                         shell=shell,
