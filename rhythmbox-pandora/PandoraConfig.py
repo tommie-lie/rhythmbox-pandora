06d2ff0cf3431029194ef514d2e87290d89081e2
Thomas Liebetraut <thomas@tommie-lie.de>
2017-04-25 12:23:42 +0200

    Add preferences dialog to set account data

--- rhythmbox-pandora/PandoraConfig.py
+++ rhythmbox-pandora/PandoraConfig.py
@@ -1,10 +1,26 @@
 from gi.repository import GObject, PeasGtk, Gtk
+import rb
+
+from .PandoraAccount import PandoraAccount
 
 class PandoraConfig(GObject.Object, PeasGtk.Configurable):
-    object = GObject.property(type=GObject.Object)
-    
     def __init__(self):
         super(PandoraConfig, self).__init__()
+        self.builder = Gtk.Builder() 
     
     def do_create_configure_widget(self):
-        return None
+        self.builder.add_from_file(rb.find_plugin_file(self, "data/pandora-prefs.ui"))
+        self.builder.connect_signals(self)
+        
+        (username, password) = PandoraAccount.get().get_credentials()
+        username_entry = self.builder.get_object("username-entry")
+        username_entry.set_text(username)
+        password_entry = self.builder.get_object("password-entry")
+        password_entry.set_text(password)
+
+        return self.builder.get_object("pandora-prefs")
+    
+    def on_login_data_changed(self, widget, event):
+        username = self.builder.get_object("username-entry").get_text()
+        password = self.builder.get_object("password-entry").get_text()
+        PandoraAccount.get().set_credentials(username, password)
